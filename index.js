9d2e9c4cb7d4de64a82e091de95132db65324d91
Frank Dillon <dillonf2@outlook.com>
2023-02-23 17:39:06 -0500

    completed advanced first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = scubaArray =>{
+    return scubaArray.slice(0,2)
+}
+
+const returnLastTwoDrivers=driverArray=>{
+    return driverArray.slice(driverArray.length-2,driverArray.length+1)
+}
+const selectingDrivers=[
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+const createFareMultiplier = value => {
+    return function(fare){
+        return value*fare
+    }
+}
+const fareDoubler = fare => fare*2
+const fareTripler = fare => fare*3
+
+function selectDifferentDrivers(array,function1){
+    if (function1===returnFirstTwoDrivers){
+       return returnFirstTwoDrivers(array)
+    } else if (function1===returnLastTwoDrivers){
+        return returnLastTwoDrivers(array)
+    }
+}
